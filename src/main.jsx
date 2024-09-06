c1b17bd0f6b03478423019ae48660efd8f15fd87
Ahmad Suyuti <ahmadsuyutisyauqi@gmail.com>
2024-09-06 18:45:52 +0700

    fix tittle and home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,92 +1,83 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/home';
-import FilterProducts from './components/Filter/Filter';
-import PaymentJapanPage from './pages/payment/japan';
-import LoginPage from './pages/login';
-import DashboardUser from './pages/dasboard-user';
-import DashboardAdmin from './pages/dashboard-admin';
-import PaymentNagataPage from './pages/payment/nagata';
-import PaymentAmericanPage from './pages/payment/american';
-import FormRegisterPage from './pages/form-register';
-import FormLupaPw from './components/Fragments/FormLupaPw';
-import AboutUs from './pages/about-us';
-import JenisKain from './pages/jenis-kain';
-import PenjelasanKain from './pages/penjelasan-kain';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/home";
+import FilterProducts from "./components/Filter/Filter";
+import PaymentJapanPage from "./pages/payment/japan";
+import LoginPage from "./pages/login";
+import DashboardUser from "./pages/dasboard-user";
+import DashboardAdmin from "./pages/dashboard-admin";
+import PaymentNagataPage from "./pages/payment/nagata";
+import PaymentAmericanPage from "./pages/payment/american";
+import FormRegisterPage from "./pages/form-register";
+import FormLupaPw from "./components/Fragments/FormLupaPw";
+import AboutUs from "./pages/about-us";
+import JenisKain from "./pages/jenis-kain";
+import PenjelasanKain from "./pages/penjelasan-kain";
 
 const Router = createBrowserRouter([
   {
-    path: '/',
-    element: <h1>Hello World</h1>,
+    path: "/",
+    element: <HomePage />,
   },
   {
-    path: '/home',
-    element: <HomePage/>,
-  },
-  {
-    path: '/payment',
+    path: "/payment",
     children: [
       {
-        path: 'japan',
-        element: <PaymentJapanPage/>,
+        path: "japan",
+        element: <PaymentJapanPage />,
       },
       {
-        path: 'nagata',
-        element: <PaymentNagataPage/>,
+        path: "nagata",
+        element: <PaymentNagataPage />,
       },
       {
-        path: 'american',
-        element: <PaymentAmericanPage/>,
+        path: "american",
+        element: <PaymentAmericanPage />,
       },
-    ]
+    ],
   },
   {
-    path: '/login',
-    element: <LoginPage/>,
+    path: "/login",
+    element: <LoginPage />,
   },
   {
-    path: '/register',
-    element: <FormRegisterPage/>,
+    path: "/register",
+    element: <FormRegisterPage />,
   },
   {
-    path: '/lupa-password',
-    element: <FormLupaPw/>,
+    path: "/lupa-password",
+    element: <FormLupaPw />,
   },
   {
-    path: '/dashboard-user',
-    element: <DashboardUser/>,
+    path: "/dashboard-user",
+    element: <DashboardUser />,
   },
   {
-    path: '/dashboard-admin',
-    element: <DashboardAdmin/>,
+    path: "/dashboard-admin",
+    element: <DashboardAdmin />,
   },
   {
-    path: '/about-us',
-    element: <AboutUs/>,
+    path: "/about-us",
+    element: <AboutUs />,
   },
   {
-    path: '/jenis-kain',
-    element: <JenisKain/>,
+    path: "/jenis-kain",
+    element: <JenisKain />,
   },
   {
-    path: '/penjelasan-kain',
-    element: <PenjelasanKain/>,
+    path: "/penjelasan-kain",
+    element: <PenjelasanKain />,
   },
-])
-
+]);
 
 function App() {
-  return (
-    <RouterProvider router={Router} />
-  );
+  return <RouterProvider router={Router} />;
 }
 
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-<App />
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
